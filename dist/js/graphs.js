0e27508cd7a2ca4b6e4af1cb86984de9eff2e8a7
arunsathya1309 <arun.sathyamurthy@gmail.com>
2018-10-01 12:01:54 -0700

    Added cfish

--- dist/js/graphs.js
+++ dist/js/graphs.js
@@ -23,7 +23,7 @@ var evalChartData = {
     data: [
     ]
   }, {
-    label: 'Live [ASMFISH]',
+    label: 'Live [CFISH]',
     lineTension: 0,
     borderColor: '#007bff',
     backgroundColor: '#007bff',
